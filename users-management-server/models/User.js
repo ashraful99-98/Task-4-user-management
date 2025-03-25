b1efd925d50b81234572e23d8c598f834d5b9c81
ashraful99-98 <ashrafulkamran98@gmail.com>
2025-03-25 16:35:53 +0600

    added all

--- users-management-server/models/User.js
+++ users-management-server/models/User.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
     {
         name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
+        // email: { type: String, required: true, unique: true },
+        email: { type: String, unique: true },
         password: { type: String, required: true },
         lastLogin: { type: Date, default: null },
         isBlocked: { type: Boolean, default: false },
